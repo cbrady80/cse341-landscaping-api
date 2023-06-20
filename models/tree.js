75240604c868876bd5c3f4679f7def9426be3781
exagent957 <exagent957@gmail.com>
2023-06-20 13:21:59 -0500

    validation for url

--- models/tree.js
+++ models/tree.js
@@ -43,15 +43,6 @@ const treeSchema = new mongoose.Schema({
   imageURL: {
     type: String,
     required: false,
-    // validate: {
-    //   validator: (value) =>
-    //     validator.isURL(value, {
-    //       protocols: ['http', 'https', 'ftp'],
-    //       require_tld: true,
-    //       require_protocol: true,
-    //     }),
-    //   message: 'Must be a Valid URL',
-    // },
   },
 });
 
