02de17955bd855a2f477cddd40ae9d5b4dd3969c
exagent957 <exagent957@gmail.com>
2023-06-30 11:19:20 -0500

    user model, routes, controller, auth

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
 const usersController = require('../controllers/users');
+const authController = require('../controllers/authController');
+// const { ensureAuth } = require('../controllers/auth');
 
-router.get('/', usersController.teamNamesRoute);
+// router.get('/', ensureAuth, usersController.getAllUsers);
+router.get('/', usersController.getAllUsers);
+
+router.get('/:id', usersController.getUserById);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+module.exports = router;
 
 module.exports = router;
