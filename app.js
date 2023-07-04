e2b51414fafe701d412d54101bbdf1a23fb7d355
wallylime <82895781+wallylime@users.noreply.github.com>
2023-07-04 15:04:39 -0600

    deleted semi-colons and uncommented out stuff

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8000;
 app.enable('trust proxy');
 
 //Logging responses in terminal - handy for dev purposes
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 //For authorizing users:
 const session = require('express-session');
@@ -29,19 +29,19 @@ app.use(
 );
 
 app
-  // .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    // );
-    // res.setHeader('Content-Type', 'application/json');
-    // res.setHeader(
-    //   'Access-Control-Allow-Methods',
-    //   'GET, POST, PUT, DELETE, OPTIONS'
-    // );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
     next();
   })
   .use('/', require('./routes')); // Calls the routes to view the data
@@ -58,8 +58,8 @@ process.on('uncaughtException', (err, origin) => {
 app.get('/login', (req, res) => {
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&prompt=consent`
-  );
-});
+  )
+})
 //That last part &prompt=consent is what helps when someone logs
 // out and then logs back in again so that it will prompt for authorization
 // again even if they are still signed in to Github somewhere.
@@ -69,10 +69,10 @@ app.get('/callback', (req, res) => {
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_CLIENT_SECRET,
     code,
-  };
+  }
   const opts = {
     headers: { accept: 'application/json' },
-  };
+  }
   axios
     .post('https://github.com/login/oauth/access_token', body, opts)
     .then((res2) => {
@@ -80,12 +80,12 @@ app.get('/callback', (req, res) => {
       res.redirect('/api-docs');
     })
     .catch((err) => res.status(500).json({ message: err.message }));
-});
+})
 //So users can logout:
 app.get('/logout', (req, res) => {
   req.session.token = null;
   res.redirect('/api-docs');
-});
+})
 
 mongodb.connectToMongo();
 
