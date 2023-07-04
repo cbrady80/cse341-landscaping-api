090171e4f19dd8b5e93ce2a0228779eaba2f6f32
wallylime <82895781+wallylime@users.noreply.github.com>
2023-07-03 21:24:41 -0600

    updated swagger

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Imports for Auth0
-// const { auth, requiresAuth } = require('express-openid-connect');
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: process.env.SESSION_SECRET,
-//   baseURL: process.env.BASE_URL,
-//   clientID: process.env.AUTH0_CLIENT_ID,
-//   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-// };
-
 // USE request for swagger
 router.use('/', require('./swagger'));
 
