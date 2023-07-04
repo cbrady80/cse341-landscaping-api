53ea2694c4c7b80187ed04a0bd5a752afc7aafae
wallylime <82895781+wallylime@users.noreply.github.com>
2023-07-03 20:35:59 -0600

    took out commented out code

--- routes/index.js
+++ routes/index.js
@@ -45,21 +45,5 @@ router.use('/grasses', require('./grasses'));
 //USE request for FLOWERS
 router.use('/flowers', require('./flowers'));
 
-//USE request for USERS
-// router.use('/users', require('./users'));
-
-//Commenting this out because it prevents login/logout routes from working
-// USE request for HOME page
-// router.use(
-//   '/',
-//   (docData = (req, res) => {
-//     let docData = {
-//       title: 'HOME PAGE - navigate to /api-docs to see api documentation',
-//       documentationURL: 'https://github.com/cbrady80/cse341-landscaping-api',
-//     };
-//     res.send(docData);
-//   })
-// );
-
 // Export
 module.exports = router;
