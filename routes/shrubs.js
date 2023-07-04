a0e65a85c3cf28173cb7efc7253b44e397f0a18c
wallylime <82895781+wallylime@users.noreply.github.com>
2023-07-04 14:26:34 -0600

    Putting auth function in shrub route

--- routes/shrubs.js
+++ routes/shrubs.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const shrubsController = require('../controllers/shrubs');
-const checkAuth = require('../controllers/authController');
+// const checkAuth = require('../controllers/authController');
+const checkAuth = (req, res, next) => {
+  try{
+    if (req.session.token) {
+      next();
+    }
+    else {
+      throw new Error("Please log in.");
+    }
+  }
+  catch(err) {res.status(500).json({ message: err.message })};
+}
 
 //Get all shrubs in database
 router.get('/', shrubsController.getAllShrubs);
@@ -11,10 +22,10 @@ router.get('/sunlight/:sunlight', shrubsController.getShrubsByLightRequirement);
 //Get a specific shrub by id
 router.get('/:id', shrubsController.getShrubById);
 
-router.post('/addShrub', checkAuth.auth, shrubsController.addShrub);
+router.post('/addShrub', checkAuth, shrubsController.addShrub);
 
-router.put('/update/:id', checkAuth.auth, shrubsController.updateShrub);
+router.put('/update/:id', checkAuth, shrubsController.updateShrub);
 
-router.delete('/delete/:id', checkAuth.auth, shrubsController.deleteShrub);
+router.delete('/delete/:id', checkAuth, shrubsController.deleteShrub);
 
 module.exports = router;
